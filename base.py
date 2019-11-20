773096da1e5847498647480b1f3aef8b62ab8a01
Kaushal1011 <kaushalpatil10@gmail.com>
2019-11-20 12:53:21 +0530

    edited visualise.py

--- base.py
+++ base.py
@@ -1,3 +1,11 @@
+###############################################################################
+#                                                                             #
+#                                                                             #
+#      BASE MODULE                                                            #
+#                                                                             #
+#                                                                             #
+###############################################################################
+
 from enum import Enum
 _SHOULD_LOG_SCOPE = False  # see '--scope' command line option
 _SHOULD_LOG_STACK = False  # see '--stack' command line option
